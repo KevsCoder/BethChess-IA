12da44fe908906986f534b61d1e6a93b65cad612
KevsCoder <kvalenzuela97@outlook.es>
2020-11-25 22:44:21 -0700

    2. Profundidad de 2 jugadas

--- js/IA.js
+++ js/IA.js
@@ -1,11 +1,12 @@
 var board = null
 var game = new Chess()
+var positionCount = 0
 
 function onDragStart(source, piece, position, orientation) {
     // do not pick up pieces if the game is over
 
- 
-    
+
+
     if (game.game_over()) {
         return false
     }
@@ -13,57 +14,108 @@ function onDragStart(source, piece, position, orientation) {
     // only pick up pieces for White
     if (piece.search(/^b/) !== -1) return false
 
- 
+
 }
 
 function renderMoveHistory(moves) {
     var historyElement = $('#move-history').empty();
     historyElement.empty();
     for (var i = 0; i < moves.length; i = i + 2) {
-        historyElement.append('<span>' + moves[i] + ' ' + ( moves[i + 1] ? moves[i + 1] : ' ') + '</span><br>')
+        historyElement.append('<span>' + moves[i] + ' ' + (moves[i + 1] ? moves[i + 1] : ' ') + '</span><br>')
     }
     historyElement.scrollTop(historyElement[0].scrollHeight);
 
 }
 
-function makeRandomMove() {
+function makeBestMove() {
 
 
-    
-    var possibleMoves = game.moves()
 
-    // game over
-    if (possibleMoves.length === 0) return
-
-    var bestValue = -1000000;
-    var bestMove = null;
-  
-    for(var i=0;i<possibleMoves.length;i++){
-        var moveIA = possibleMoves[i]
-        game.move(moveIA)
-
-
-        var boardValue = -evaluateBoard(game.board())
-        game.undo()
-
-        if(boardValue>bestValue){
-            bestValue = boardValue
-            bestMove = moveIA
-        }
-        
+    if (game.game_over()) {
+        alert("Se acabo el juego")
     }
+
+    positionCount = 0
+    var depth = 2
+    // var d = new Date().getTime()
+    var bestMove = minimaxRoot(depth, game, true)
+    // var d2 = new Date().getTime()
+    // var moveTime = (d2 - d)
+    // var positionPerS = (positionCount * 1000 / moveTime)
 
     game.move(bestMove)
     board.position(game.fen())
+    return bestMove
 
 
 
 
 
 
-   
 
-  
+}
+
+function minimaxRoot(depth, game, isMaximisingPlayer) {
+
+    var gameMoves = game.moves()
+    var bestValue = -9999999
+    var bestMove = null
+
+    for (var i = 0; i < gameMoves.length; i++) {
+        var newMove = gameMoves[i]
+        game.move(newMove)
+
+        var value = minimax(depth - 1, game, !isMaximisingPlayer)
+        game.undo()
+
+        if (value >= bestValue) {
+            bestValue = value
+            bestMove = newMove
+        }
+    }
+
+    return bestMove
+
+}
+
+function minimax(depth, game, isMaximisingPlayer) {
+    positionCount++
+    if (depth === 0) {
+
+        if(game.in_checkmate()){
+            return -9999999
+        }
+
+        return -evaluateBoard(game.board())
+    }
+
+    var newGameMoves = game.moves()
+    var bestMove = null
+
+    if (isMaximisingPlayer) {
+        var bestValue = -10000
+
+        for(var i=0;i<newGameMoves.length;i++){
+            game.move(newGameMoves[i])
+            bestMove = Math.max(bestValue, minimax(depth-1, game, !isMaximisingPlayer))
+            game.undo()
+        }
+
+        return bestMove
+
+    } else {
+        var bestValue = 100000
+
+        for(var i=0;i<newGameMoves.length;i++){
+            game.move(newGameMoves[i])
+            bestMove = Math.min(bestValue, minimax(depth-1, game, isMaximisingPlayer))
+            game.undo()
+        }
+
+        return bestMove
+    }
+
+
 }
 
 
@@ -77,53 +129,53 @@ function onDrop(source, target) {
 
     // illegal move
     if (move === null) return 'snapback'
-    
+
     // make random legal move for black
-    window.setTimeout(makeRandomMove, 250)
-   
+    window.setTimeout(makeBestMove, 250)
+
 }
 
 function onSnapEnd() {
     board.position(game.fen())
 }
 
-function getPieceValue(piece){
-    if(piece === null){
+function getPieceValue(piece) {
+    if (piece === null) {
         return 0
     }
 
-    if(piece.type === 'p'){
-        return piece.color === 'w' ? 10:-10
+    if (piece.type === 'p') {
+        return piece.color === 'w' ? 10 : -10
     }
 
-    if(piece.type === 'n'){
-        return piece.color === 'w' ? 30:-30
+    if (piece.type === 'n') {
+        return piece.color === 'w' ? 30 : -30
     }
 
-    
-    if(piece.type === 'b'){
-        return piece.color === 'w' ? 31:-31
+
+    if (piece.type === 'b') {
+        return piece.color === 'w' ? 31 : -31
     }
 
-    if(piece.type === 'r'){
-        return piece.color === 'w' ? 50:-50
+    if (piece.type === 'r') {
+        return piece.color === 'w' ? 50 : -50
     }
 
-    
-    if(piece.type === 'q'){
-        return piece.color === 'w' ? 90:-90
+
+    if (piece.type === 'q') {
+        return piece.color === 'w' ? 90 : -90
     }
 
-    
-    if(piece.type === 'k'){
-        return piece.color === 'w' ? 1000:-1000
+
+    if (piece.type === 'k') {
+        return piece.color === 'w' ? 1000 : -1000
     }
 }
 
-function evaluateBoard(board){
+function evaluateBoard(board) {
     var totalEvaluation = 0
-    for(i = 0; i < 8; i++){
-        for(j = 0; j < 8; j++){
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
             totalEvaluation = totalEvaluation + getPieceValue(board[i][j]);
         }
     }
